846976147ca5c276beaa47ab0876664abd1e8692
vadossus <157449023+vadossus@users.noreply.github.com>
2024-04-29 13:50:18 +0500

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(53, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label1.Location = new System.Drawing.Point(355, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HashTable";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить диск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить диск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(423, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Добавить песню";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(423, 371);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 25);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Удалить песню";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(423, 85);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(346, 238);
+            this.listBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Диск и исполнитель";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(573, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Название музыки и исполнитель";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(576, 343);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 394);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Поиск";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(576, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Поиск";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(576, 394);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(615, 420);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Поиск";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(270, 420);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Поиск";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(288, 456);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(244, 43);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Отсортировать листы";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Проект Вадима Горового ПР_22";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Security.Policy;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Hashtable hs = new Hashtable();
+        Hashtable hs2 = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("Ошибка: Пустые строка в поле");
+                    return;
+                }
+                else
+                {
+                    string disk_author = textBox1.Text;
+
+                    if (!listBox1.Items.Contains(disk_author))
+                    {
+                        hs.Add(hs.Count + 1, disk_author);
+                        listBox1.Items.Add(hs[hs.Count]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Данный диск уже присутствует");
+                        return;
+                    }
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Ошибка: Данный элемент уже добавлен");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBox4.Text))
+                {
+                    MessageBox.Show("Ошибка: Пустые строка в поле");
+                    return;
+                }
+                else
+                {
+                    string music = textBox4.Text;
+
+                    if (!listBox2.Items.Contains(music))
+                    {
+                        hs2.Add(hs2.Count + 1, music);
+                        listBox2.Items.Add(hs2[hs2.Count]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Данная песня уже присутствует");
+                        return;
+                    }
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Ошибка: Данный элемент уже добавлен");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                hs.Remove(listBox1.SelectedIndex + 1);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Выберите элемент для удаления");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                hs2.Remove(listBox2.SelectedIndex + 1);
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Выберите элемент для удаления");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string name = textBox2.Text;
+
+            List<string> symma = new List<string>();
+            foreach (string value in hs.Values)
+            {
+                if (value.StartsWith(name))
+                {
+                    symma.Add(value);
+                }
+            }
+
+            listBox1.ClearSelected();  
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (symma.Contains(listBox1.Items[i].ToString()))
+                {
+                    listBox1.SetSelected(i, true); 
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string name = textBox3.Text;
+
+            List<string> symma1 = new List<string>();
+            foreach (string value in hs2.Values)
+            {
+                if (value.StartsWith(name))
+                {
+                    symma1.Add(value);
+                }
+            }
+
+            listBox2.ClearSelected();
+
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                if (symma1.Contains(listBox2.Items[i].ToString()))
+                {
+                    listBox2.SetSelected(i, true);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0 && listBox2.Items.Count <= 0)
+            {
+                MessageBox.Show("Нечего сортировать");
+                return;
+            }
+            else if (listBox1.Items.Count > 0)
+            {
+                List<string> values = new List<string>(hs.Values.Cast<string>());
+
+                values.Sort();
+
+                listBox1.Items.Clear();
+
+                foreach (string value in values)
+                {
+                    listBox1.Items.Add(value);
+                }
+            }
+            else if (listBox2.Items.Count > 0)
+            {
+                List<string> values = new List<string>(hs2.Values.Cast<string>());
+
+                values.Sort();
+
+                listBox2.Items.Clear();
+
+                foreach (string value in values)
+                {
+                    listBox2.Items.Add(value);
+                }
+            }
+
+
+        }
+    }
+}
